8584a7a0b88ca916f6ce70f987b67bd700bf8934
Julio Jimenez <julio@julioj.com>
2023-11-17 21:34:51 -0500

    boundaries
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- rules.rkt
+++ rules.rkt
@@ -9,8 +9,8 @@
 ;; —————————————————————————————————
 ;; import and implementation section
 
-(define aws-id            #px"(AKIA|ABIA|ACCA)[0-9A-Z]{16}")
-(define aws-secret        #px"[A-Za-z0-9+/]{40}[^A-Za-z0-9+/]{0,1}")
-(define aws-session-id    #px"ASIA[0-9A-Z]{16}")
-(define aws-session-token #px"[A-Za-z0-9+=/]{41,1000}[^A-Za-z0-9+=/]{0,1}")
-(define gcp-api-key       #px"AIza[A-Za-z0-9_-]{35}")
+(define aws-id            #px"\\b(AKIA|ABIA|ACCA)[0-9A-Z]{16}\\b")
+(define aws-secret        #px"\\b[A-Za-z0-9+/]{40}[^A-Za-z0-9+/]{0,1}\\b")
+(define aws-session-id    #px"\\bASIA[0-9A-Z]{16}\\b")
+(define aws-session-token #px"\\b[A-Za-z0-9+=/]{41,1000}[^A-Za-z0-9+=/]{0,1}\\b")
+(define gcp-api-key       #px"\\bAIza[A-Za-z0-9_-]{35}\\b")
