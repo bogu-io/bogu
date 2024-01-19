e279aeee91b0b1e2189e5e45ccbe1dd2b8c577f9
Julio Jimenez <julio@julioj.com>
2024-01-18 23:49:46 -0500

    Fix: sudo command not found
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- scripts/darwin/install.sh
+++ scripts/darwin/install.sh
@@ -2,7 +2,7 @@
 VERSION="0.0.3"
 echo "Beginning bogu installation..."
 echo "Move bogu to /usr/local..."
-sudo mv bogu-${VERSION} /usr/local/bogu
+mv bogu-${VERSION} /usr/local/bogu
 echo "Adding bogu to PATH..."
 echo "export PATH=$PATH:/usr/local/bogu" >> $HOME/.zshrc
 source $HOME/.zshrc
