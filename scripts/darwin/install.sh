3a11bfa8a00ce0fb7fc6899133d7b163d0218c1b
Julio Jimenez <julio@julioj.com>
2024-01-18 23:42:47 -0500

    Fix Path Script For Darwin
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- scripts/darwin/install.sh
+++ scripts/darwin/install.sh
@@ -4,8 +4,8 @@ echo "Beginning bogu installation..."
 echo "Move bogu to /usr/local..."
 sudo mv bogu-${VERSION} /usr/local/bogu
 echo "Adding bogu to PATH..."
-echo "export PATH=$PATH:/usr/local/bogu" >> $HOME/.profile
-source $HOME/.profile
+echo "export PATH=$PATH:/usr/local/bogu" >> $HOME/.zshrc
+source $HOME/.zshrc
 rm bogu-${VERSION}-darwin-arm64.zip
 echo "Installation complete!"
 bogu --version
