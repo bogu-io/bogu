e35d5603e4d71b2cac74b8657e2d1fd0045d3884
Julio Jimenez <julio@julioj.com>
2024-02-03 19:49:40 -0500

    Shannon Entropy
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- utils.rkt
+++ utils.rkt
@@ -21,3 +21,17 @@
       (string-contains? file-contents "client_email")
       (string-contains? file-contents "client_id")) #t]
     [else #f]))
+
+;; Checks Shannon entropy of a string
+(define (shannon str)
+  (define len (string-length str))
+  (define freq (make-hash))
+  (for ([i (in-range len)])
+    (hash-update! freq (string-ref str i) (add1 0)))
+  (define entropy 0.0)
+  (for ([count (in-hash-values freq)])
+    (define p (/ count len))
+    (define term (* p (log p 2)))
+    (set! entropy (+ entropy term)))
+  (set! entropy (- 0 entropy))
+  entropy)
