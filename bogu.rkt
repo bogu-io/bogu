f7a2dcfb7623dd63295826fd9650699839423cc5
Julio Jimenez <julio@julioj.com>
2024-02-28 12:42:40 -0500

    --symlink
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- bogu.rkt
+++ bogu.rkt
@@ -9,13 +9,15 @@
   "local.rkt"
   "github.rkt"
   "parser.rkt"
-  "strings.rkt")
+  "strings.rkt"
+  "utils.rkt")
 
 (define (main args)
   (displayln (string-append "Bogu " version-slug))
   (cond [(= (vector-length args) 0) (displayln help-text)])
   (cond [(> (string-length (local-path)) 0) (local-scan (local-path))])
   (cond [(> (string-length (github-owner)) 0) (github-scan (github-owner))])
+  (cond [(symlink) (symbolic-link)])
   (cond [(version) (displayln version-slug)])
   (values))
 
