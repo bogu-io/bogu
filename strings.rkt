ab04d2fc831bc33ff63112ab0692046386b1c90b
Julio Jimenez <julio@julioj.com>
2024-03-26 22:13:37 -0400

    Secrets Count
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- strings.rkt
+++ strings.rkt
@@ -9,7 +9,7 @@
 ;; —————————————————————————————————
 ;; import and implementation section
 
-(define version-slug "v0.0.12")
+(define version-slug "v0.0.13")
 
 (define help-text (format "Bogu - ~a
 The Secret Scanner
