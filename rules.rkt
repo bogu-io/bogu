844462e3bf7435cabf3cef8adeebca78b83dba3f
Julio Jimenez <julio@julioj.com>
2024-04-04 23:44:37 -0400

    Lumos API Keys
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- rules.rkt
+++ rules.rkt
@@ -14,3 +14,4 @@
 (define aws-session-id    #px"\\bASIA[0-9A-Z]{16}\\b")
 (define aws-session-token #px"\\b[A-Za-z0-9+=/]{41,1000}[^A-Za-z0-9+=/]{0,1}\\b")
 (define gcp-api-key       #px"\\bAIza[A-Za-z0-9_-]{35}\\b")
+(define lumos-api-key     #px"\\blsk_(.{44})\\b")
